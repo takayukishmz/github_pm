8662da490d98a4d492f30dc05f0493c01aeaeee2
takayukishmz <t.shimizu0713@gmail.com>
2016-05-21 16:37:06 +0900

    define interfaces

--- lib/github_pm.rb
+++ lib/github_pm.rb
@@ -1,5 +1,18 @@
 require "github_pm/version"
 
 module GithubPm
-  # Your code goes here...
+  def self.greet
+    p 'Hello world!'
+  end
+
+  def self.summarize_pull_request
+  end
+
+  def self.milestones
+
+  end
+
+  def self.issues
+
+  end
 end
