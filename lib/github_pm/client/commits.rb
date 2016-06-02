b9fc54b779d1035631a3b63608f679773b4e42bb
takayukishmz <t.shimizu0713@gmail.com>
2016-06-02 16:36:37 +0900

    start to implement client/commits.rb

---
+++ lib/github_pm/client/commits.rb
@@ -0,0 +1,13 @@
+module GithubPm
+  class Client
+    module Commits
+
+      def total_contributes(options)
+        # {
+        #   add: 1234, #line
+        #   remove: 123,
+        # }
+      end
+    end
+  end
+end
